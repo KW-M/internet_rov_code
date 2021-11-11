7040fe007fbcb147ffa085be110551a5b086a4e2
Kyle Worcester-Moore <25kylworc@gmail.com>
2021-11-10 19:48:19 -0800

    more tests

--- python/pihat_motor_driver_test.py
+++ python/pihat_motor_driver_test.py
@@ -10,14 +10,14 @@ kit = MotorKit(i2c=board.I2C())
 
 while (True):
     for i in range(50):
-        kit.motor4.throttle = (i-25) / 50
-        kit.motor3.throttle = (i-25) / 50
-        kit.motor2.throttle = (i-25) / 50
+        # kit.motor4.throttle = (i-25) / 50
+        # kit.motor3.throttle = (i-25) / 50
+        # kit.motor2.throttle = (i-25) / 50
         kit.motor1.throttle = (i-25) / 50
         time.sleep(0.1)
     kit.motor1.throttle = 0
-    kit.motor2.throttle = 0
-    kit.motor3.throttle = 0
-    kit.motor4.throttle = 0
+    # kit.motor2.throttle = 0
+    # kit.motor3.throttle = 0
+    # kit.motor4.throttle = 0
     print("0, sleeping for 2...")
     time.sleep(2)
