ab380c027f748760ee10769b07b13abe7d285ddd
Kyle Worcester-Moore <25kylworc@gmail.com>
2021-12-15 16:54:50 -0800

    fixed ip fix

--- new_config_files/add_fixed_ip.sh
+++ new_config_files/add_fixed_ip.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# Montors the Pi's IP addresses for changes (because a network interface came online/offline)
-
-ip mon addr | sed -nu -r 's/.*[[:digit:]]+:[[:space:]]+([^[:space:]]+).*/\1/p' | while read iface; do
+# Montors the Pi's network interfaces for changes (because a network interface came online/offline)
+# uses awk to split the output of the ip monitor link on ': ' and then output the second column if it
+# is not empty. In this case, the second column is the interface name.
+# last we assign that to the variable iface with read and add the fixed ip address to that.
+ip monitor link | awk -W interactive -F ': ' '{if ($2) print $2;}' | while read iface; do
     echo "Adding fixed IP 192.168.0.88 to ${interface}."
     sudo ip address add 192.168.0.88 dev ${interface}
 done
