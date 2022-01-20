7c6cd4e4931d62dc283e50d562f923cbdc96aba6
Kyle Worcester-Moore <25kylworc@gmail.com>
2022-01-20 13:03:30 -0800

    test

--- peerjs-webrtc-libcamera/connection.go
+++ peerjs-webrtc-libcamera/connection.go
@@ -137,6 +137,31 @@ func pipeVideoToStream(done chan bool) error {
 }
 
 func setupWebrtcConnection(done chan bool) {
+
+	peer1, _ := peer.NewPeer("peer1", peer.NewOptions())
+	defer peer1.Close()
+
+	peer2, _ := peer.NewPeer("peer2", peer.NewOptions())
+	defer peer2.Close()
+
+	peer2.On("connection", func(data interface{}) {
+		conn2 := data.(*peer.DataConnection)
+		conn2.On("data", func(data interface{}) {
+			// Will print 'hi!'
+			log.Printf("Received: %#v: %s\n", data, data)
+		})
+	})
+
+	conn1, _ := peer1.Connect("peer2", nil)
+	conn1.On("open", func(data interface{}) {
+		for {
+			conn1.Send([]byte("hi!"), false)
+			<-time.After(time.Millisecond * 1000)
+		}
+	})
+
+	select {}
+
 	// setup peerjs-go
 	peerjsOpts := peerjs.NewOptions()
 	peerjsOpts.Debug = 3
