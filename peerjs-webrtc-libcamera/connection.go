1d855ab56d851ef849bc1476dffc9fb9b69d3aab
Kyle Worcester-Moore <25kylworc@gmail.com>
2022-01-20 13:12:13 -0800

    test

--- peerjs-webrtc-libcamera/connection.go
+++ peerjs-webrtc-libcamera/connection.go
@@ -178,6 +178,14 @@ func setupWebrtcConnection(done chan bool) {
 	rovWebsocketPeer, _ := peerjs.NewPeer("SROV", peerjsOpts)
 	defer rovWebsocketPeer.Close() // close the websocket connection when this function exits
 
+	conn1, _ := rovWebsocketPeer.Connect("peer2", nil)
+	conn1.On("open", func(data interface{}) {
+		for {
+			conn1.Send([]byte("hi!"), false)
+			<-time.After(time.Millisecond * 1000)
+		}
+	})
+
 	// rovWebsocketPeer.On("connection", func(dataConn interface{}) {
 
 	// 	log.Println("Got connection!")
