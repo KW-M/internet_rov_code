14b543a355d2dda3f8c8f2ac7f63c31fcfaf01c5
Kyle Worcester-Moore <25kylworc@gmail.com>
2022-01-22 21:25:25 -0800

    revert test

--- peerjs-webrtc-libcamera/connection.go
+++ peerjs-webrtc-libcamera/connection.go
@@ -5,8 +5,9 @@ import (
 	// "io/ioutil"
 	// "errors"
 	"bufio"
-	"io"
 	"log"
+	"net"
+	"syscall"
 
 	// "flag"
 	"fmt"
@@ -21,7 +22,6 @@ import (
 	peerjs "github.com/KW-M/peerjs-go"
 	webrtc "github.com/pion/webrtc/v3"
 	"github.com/pion/webrtc/v3/pkg/media"
-	"github.com/pion/webrtc/v3/pkg/media/h264reader"
 )
 
 const (
@@ -34,117 +34,214 @@ var (
 
 func pipeVideoToStream(done chan bool) error {
 	// Startup libcamera-vid command to get the video data from the camera exposed (locally) on a http/tcp port
-	cmd := exec.Command("libcamera-vid", "--width", "640", "--height", "480", "--framerate", "16", "--bitrate", "8000000", "--codec", "h264", "--profile", "baseline", "--level", "4.2", "--inline", "1", "--flush", "1", "--timeout", "0","--nopreview", "1", "--output", "-") //"--listen", "1", "--output", "tcp://0.0.0.0:8585")
+	cmd := exec.Command("libcamera-vid", "--width", "640", "--height", "480", "--framerate", "16", "--bitrate", "8000000", "--codec", "h264", "--profile", "baseline", "--level", "4.2", "--inline", "1", "--flush", "1", "--timeout", "0","--nopreview", "1", "--listen", "1", "--output", "tcp://0.0.0.0:8585")
 	fmt.Println(cmd.Args)
 
-	dataPipe, err := cmd.StdoutPipe()
-	if err != nil {
-		log.Fatal("could not create named pipe. ", err)
-	}
+	// print out the stdout output of the command in a seperate go routine
+	go func() {
+		dataPipe, err := cmd.StdoutPipe()
+		if err != nil {
+			log.Fatal("could not create named pipe. ", err)
+		}
+		scanner := bufio.NewScanner(dataPipe)
+		for {
+			scanner.Scan()
+			fmt.Printf("[libcamera-vid] > %s\n", scanner.Text())
+		}
+	}()
+
 
 	if err := cmd.Start(); err != nil {
 		return err
 	}
 
-	// print out the initial 50 lines of stdout output of the command
-	scanner := bufio.NewScanner(dataPipe)
-	for i := 0; i < 50; i++ {
-		scanner.Scan()
-        fmt.Printf("\t > %s\n", scanner.Text())
-    }
 
+	fmt.Println(cmd.Args)
 
-	// framebuffer := make(chan []byte, 60)
+	// dataPipe, err := cmd.StdoutPipe()
+	// if err != nil {
+	// 	log.Fatal("could not create named pipe. ", err)
+	// }
 
-	h264, h264Err := h264reader.NewReader(dataPipe)
-	if h264Err != nil {
-		log.Println("h264reader Initilization Error")
-		panic(h264Err)
+	if err := cmd.Start(); err != nil {
+		return err
 	}
 
+	time.Sleep(time.Second * 5)
+	fmt.Println("Hello, playground")
+
+	// connect to site
+	conn, err := net.Dial("tcp", "127.0.0.1:8585")
+	if err != nil {
+		fmt.Printf("failed to connect to video tcp: %s\n", err)
+		return err
+	}
+
+	fmt.Printf("connected to video tcp\n")
+
+	framebuffer := make(chan []byte, 60)
+
+	// continuously read from the tcp connection and write the video data to the framebuffer variable in a separate goroutine
 	go func() {
-		// Send our video a frame at a time. Pace our sending so we send it at the same speed it should be played back as.
-		// This isn't required since the video is timestamped, but we will such much higher loss if we send all at once.
-		//
-		// It is important to use a time.Ticker instead of time.Sleep because
-		// * avoids accumulating skew, just calling time.Sleep didn't compensate for the time spent parsing the data
-		// * works around latency issues with Sleep (see https://github.com/golang/go/issues/44343)
-		spsAndPpsCache := []byte{}
-		ticker := time.NewTicker(h264FrameDuration)
-		for ; true; <-ticker.C {
-			nal, h264Err := h264.NextNAL()
-			if h264Err == io.EOF {
-				fmt.Printf("All video frames parsed and sent")
-				break
-			}
-			if h264Err != nil {
-				log.Println("h264reader Decode Error", h264Err)
-				continue
-			}
+		for {
+			select {
+			case <-done: // stop the goroutine because a signal was sent on the 'done' channel from the main.go file to clean up because program is exiting or somthin.
+				return
+			default:
+				framebytes := make([]byte, 600000)
+				// n, err := dataPipe.Read(framebytes)
+				// if err != nil {
+				// 	log.Println("could not read pipe. ", err)
+				// }
 
-			nal.Data = append([]byte{0x00, 0x00, 0x00, 0x01}, nal.Data...)
+				nread, err := conn.Read(framebytes)
+				if err != nil {
+					fmt.Printf("failed to read from socket: %s\n", err)
+					return
+				}
+				// fmt.Printf("bytes read: %d\n", nread)
 
-			if nal.UnitType == h264reader.NalUnitTypeSPS || nal.UnitType == h264reader.NalUnitTypePPS {
-				spsAndPpsCache = append(spsAndPpsCache, nal.Data...)
-				continue
-			} else if nal.UnitType == h264reader.NalUnitTypeCodedSliceIdr {
-				nal.Data = append(spsAndPpsCache, nal.Data...)
-				spsAndPpsCache = []byte{}
-			}
-
-			if h264WriteErr := videoTrack.WriteSample(media.Sample{Data: nal.Data, Duration: time.Second}); h264WriteErr != nil {
-				log.Println("Error writing h264 video track sample: ", h264WriteErr)
-				// panic(h264WriteErr)
-
+				framebuffer <- framebytes[:nread]
 			}
 		}
 	}()
-	// continuously read from the tcp connection and write the video data to the framebuffer variable in a separate goroutine
-	// go func() {
-	// 	for {
-	// 		select {
-	// 		case <-done: // stop the goroutine because a signal was sent on the 'done' channel from the main.go file to clean up because program is exiting or somthin.
-	// 			return
-	// 		default:
-	// 			framebytes := make([]byte, 600000)
-	// 			// n, err := dataPipe.Read(framebytes)
-	// 			// if err != nil {
-	// 			// 	log.Println("could not read pipe. ", err)
-	// 			// }
 
-	// 			nread, err := conn.Read(framebytes)
-	// 			if err != nil {
-	// 				fmt.Printf("failed to read from socket: %s\n", err)
-	// 				return
-	// 			}
-	// 			// fmt.Printf("bytes read: %d\n", nread)
+	// continuously sample video data from framebuffer (if present) and write it to the webrtc track in a separate goroutine
+	go func() {
+		for {
+			select {
+			case <-done: // stop the goroutine because a signal was sent on the 'done' channel from the main.go file to clean up because program is exiting or somthin.
+				if err := cmd.Process.Signal(syscall.SIGTERM); err != nil {
+					log.Println("Failed to kill camera process. ", err)
+				}
+				return
+			case frame := <-framebuffer: // if new data is in the framebuffer, grab it, (delete from buffer?), and use it in the media sample
 
-	// 			framebuffer <- framebytes[:nread]
-	// 		}
-	// 	}
-	// }()
-
-	// // continuously sample video data from framebuffer (if present) and write it to the webrtc track in a separate goroutine
-	// go func() {
-	// 	for {
-	// 		select {
-	// 		case <-done: // stop the goroutine because a signal was sent on the 'done' channel from the main.go file to clean up because program is exiting or somthin.
-	// 			if err := cmd.Process.Signal(syscall.SIGTERM); err != nil {
-	// 				log.Println("Failed to kill camera process. ", err)
-	// 			}
-	// 			return
-	// 		case frame := <-framebuffer: // if new data is in the framebuffer, grab it, (delete from buffer?), and use it in the media sample
-
-	// 			if err := videoTrack.WriteSample(media.Sample{Data: frame, Duration: time.Second}); err != nil {
-	// 				log.Fatal("could not write rtp sample. ", err)
-	// 				return
-	// 			}
-	// 		}
-	// 	}
-	// }()
+				if err := videoTrack.WriteSample(media.Sample{Data: frame, Duration: time.Second}); err != nil {
+					log.Fatal("could not write rtp sample. ", err)
+					return
+				}
+			}
+		}
+	}()
 
 	return nil
 }
+
+// func pipeVideoToStream(done chan bool) error {
+// 	// Startup libcamera-vid command to get the video data from the camera exposed (locally) on a http/tcp port
+// 	cmd := exec.Command("libcamera-vid", "--width", "640", "--height", "480", "--framerate", "16", "--bitrate", "8000000", "--codec", "h264", "--profile", "baseline", "--level", "4.2", "--inline", "1", "--flush", "1", "--timeout", "0","--nopreview", "1", "--output", "-") //"--listen", "1", "--output", "tcp://0.0.0.0:8585")
+// 	fmt.Println(cmd.Args)
+
+// 	dataPipe, err := cmd.StdoutPipe()
+// 	if err != nil {
+// 		log.Fatal("could not create named pipe. ", err)
+// 	}
+
+// 	if err := cmd.Start(); err != nil {
+// 		return err
+// 	}
+
+// 	// print out the initial 50 lines of stdout output of the command
+// 	scanner := bufio.NewScanner(dataPipe)
+// 	for i := 0; i < 50; i++ {
+// 		scanner.Scan()
+//         fmt.Printf("\t > %s\n", scanner.Text())
+//     }
+
+
+// 	// framebuffer := make(chan []byte, 60)
+
+// 	h264, h264Err := h264reader.NewReader(dataPipe)
+// 	if h264Err != nil {
+// 		log.Println("h264reader Initilization Error")
+// 		panic(h264Err)
+// 	}
+
+// 	go func() {
+// 		// Send our video a frame at a time. Pace our sending so we send it at the same speed it should be played back as.
+// 		// This isn't required since the video is timestamped, but we will such much higher loss if we send all at once.
+// 		//
+// 		// It is important to use a time.Ticker instead of time.Sleep because
+// 		// * avoids accumulating skew, just calling time.Sleep didn't compensate for the time spent parsing the data
+// 		// * works around latency issues with Sleep (see https://github.com/golang/go/issues/44343)
+// 		spsAndPpsCache := []byte{}
+// 		ticker := time.NewTicker(h264FrameDuration)
+// 		for ; true; <-ticker.C {
+// 			nal, h264Err := h264.NextNAL()
+// 			if h264Err == io.EOF {
+// 				fmt.Printf("All video frames parsed and sent")
+// 				break
+// 			}
+// 			if h264Err != nil {
+// 				log.Println("h264reader Decode Error", h264Err)
+// 				continue
+// 			}
+
+// 			nal.Data = append([]byte{0x00, 0x00, 0x00, 0x01}, nal.Data...)
+
+// 			if nal.UnitType == h264reader.NalUnitTypeSPS || nal.UnitType == h264reader.NalUnitTypePPS {
+// 				spsAndPpsCache = append(spsAndPpsCache, nal.Data...)
+// 				continue
+// 			} else if nal.UnitType == h264reader.NalUnitTypeCodedSliceIdr {
+// 				nal.Data = append(spsAndPpsCache, nal.Data...)
+// 				spsAndPpsCache = []byte{}
+// 			}
+
+// 			if h264WriteErr := videoTrack.WriteSample(media.Sample{Data: nal.Data, Duration: time.Second}); h264WriteErr != nil {
+// 				log.Println("Error writing h264 video track sample: ", h264WriteErr)
+// 				// panic(h264WriteErr)
+
+// 			}
+// 		}
+// 	}()
+// 	// continuously read from the tcp connection and write the video data to the framebuffer variable in a separate goroutine
+// 	// go func() {
+// 	// 	for {
+// 	// 		select {
+// 	// 		case <-done: // stop the goroutine because a signal was sent on the 'done' channel from the main.go file to clean up because program is exiting or somthin.
+// 	// 			return
+// 	// 		default:
+// 	// 			framebytes := make([]byte, 600000)
+// 	// 			// n, err := dataPipe.Read(framebytes)
+// 	// 			// if err != nil {
+// 	// 			// 	log.Println("could not read pipe. ", err)
+// 	// 			// }
+
+// 	// 			nread, err := conn.Read(framebytes)
+// 	// 			if err != nil {
+// 	// 				fmt.Printf("failed to read from socket: %s\n", err)
+// 	// 				return
+// 	// 			}
+// 	// 			// fmt.Printf("bytes read: %d\n", nread)
+
+// 	// 			framebuffer <- framebytes[:nread]
+// 	// 		}
+// 	// 	}
+// 	// }()
+
+// 	// // continuously sample video data from framebuffer (if present) and write it to the webrtc track in a separate goroutine
+// 	// go func() {
+// 	// 	for {
+// 	// 		select {
+// 	// 		case <-done: // stop the goroutine because a signal was sent on the 'done' channel from the main.go file to clean up because program is exiting or somthin.
+// 	// 			if err := cmd.Process.Signal(syscall.SIGTERM); err != nil {
+// 	// 				log.Println("Failed to kill camera process. ", err)
+// 	// 			}
+// 	// 			return
+// 	// 		case frame := <-framebuffer: // if new data is in the framebuffer, grab it, (delete from buffer?), and use it in the media sample
+
+// 	// 			if err := videoTrack.WriteSample(media.Sample{Data: frame, Duration: time.Second}); err != nil {
+// 	// 				log.Fatal("could not write rtp sample. ", err)
+// 	// 				return
+// 	// 			}
+// 	// 		}
+// 	// 	}
+// 	// }()
+
+// 	return nil
+// }
+
 
 func setupWebrtcConnection(done chan bool) {
 
