3688b942e96854979d636df442ddede3393f0b86
Kyle Worcester-Moore <25kylworc@gmail.com>
2022-01-28 14:26:47 -0800

    try iframe responder

--- go/main.js
+++ go/main.js
@@ -32,6 +32,188 @@ function checkInternetConnection() {
     })
 }
 
+var thisPeer = null
+var remotePeerId = null
+var peerServerOption = peerServerCloudOptions;
+var peerId = "SSROV_0"
+function connectToPeerServer(reconnect, abortConnection) {
+
+    console.log("Connecting to peerjs server...", peerServerOption);
+    thisPeer = new Peer(peerServerOption);
+
+    thisPeer.on('open', function (realPeerId) {
+        console.log("Connection to peerjs server established! our PeerID:", realPeerId);
+        reliablyConnectToROV(remotePeerId)
+    });
+    thisPeer.on('call', function (call) {
+        rovVideoConnection = call;
+        console.log('Received video call from: ' + call.peer, call);
+        call.answer(null, {
+            // sdpTransform: function (sdp) {
+            //     console.log('answer sdp: ', sdp);
+            //     return sdp;
+            // }
+        });
+        call.on('stream', function (remoteStream) {
+            console.log('Received stream from: ' + call.peer, remoteStream);
+            var video = document.getElementById('livestream');
+            // video.src = URL.createObjectURL(remoteStream);
+            video.srcObject = remoteStream;
+            video.autoplay = true
+            video.controls = false
+            video.play();
+        });
+    });
+    thisPeer.on('error', function (err) {
+        try {
+            if (err.type == 'browser-incompatible') {
+                alert('Your browser does not support some WebRTC features, please use a different or newer browser.');
+                abortConnection()
+            } else if (err.type == "peer-unavailable" && thisPeer.open) console.info("Peer unavailable")
+            else {
+                console.warn('Self Peerjs Error: ', err);
+                reconnect() //loop back and try again
+            }
+        } catch (e) {
+            console.error(e)
+            reconnect()
+        }
+    });
+    thisPeer.on('disconnected', function () {
+        console.log('Self Peerjs disconnected.');
+        console.log("attempting to reconnect to peerjs server ..")
+        thisPeer.reconnect(); // not a function
+    });
+    thisPeer.on('close', function () {
+        console.log('Self Peerjs connection closed.');
+        reconnect()
+    });
+}
+
+function reliablyConnectToPeerServer() {
+    // Using promises as a loop to avoid stack overflow: https://stackoverflow.com/questions/50421769/recursive-promises-can-cause-stack-overflow
+    return new Promise(connectToPeerServer).then(() => {
+        console.log("this peer resolve", thisPeer)
+        // the resolve() callback in the promise function above should get called whenever there is an error connecting to the rov peer, where we should try to reconnect,
+        // the reject() callback in the promise function above should get called whenever there is an error connecting to the peerjs server, where this function should exit and let the outer function take over,
+        // our strange way of looping in order to avoid call stack overflow / memory leak if this loop runs too much
+        return reliablyConnectToPeerjsServer()
+    }).catch((e) => {
+
+    })
+}
+
+
+function reliablyConnectToROV() {
+    // Using promises as a loop to avoid stack overflow when reconnecting https://stackoverflow.com/questions/50421769/recursive-promises-can-cause-stack-overflow
+    return new Promise(connectToRov).then(() => {
+        console.log("remote peer resolve", thisPeer)
+        // the resolve() callback in the promise function above should get called whenever there is an error connecting to the rov peer, where we should try to reconnect,
+        // the reject() callback in the promise function above should get called whenever there is an error connecting to the peerjs server, where this function should exit and let the outer function take over,
+        // our strange way of looping in order to avoid call stack overflow / memory leak if this loop runs too much
+        if (thisPeer == null || thisPeer.closed) return;
+        return reliablyConnectToROV()
+    }).catch(() => {
+        try {
+            if (rovDataConnection) rovDataConnection.close();
+            if (rovVideoConnection) rovVideoConnection.close();
+        } catch (e) {
+            console.warn("Error Closing rov Data Or Media connection: ", e)
+        }
+    })
+}
+
+var rovDataConnection = null;
+var rovVideoConnection = null;
+var intervalCheck = null
+
+// this is meant to be used as a new promise callback and is NOT used in the normal way, it is part of the loop in connect to rov reliably, and resolves on errors
+function connectToRov(reconnect, abortConnection) {
+    rovDataConnection = thisPeer.connect(peerId, {
+        reliable: true,
+        serialization: 'none',
+    });
+    rovDataConnection.on('open', function () {
+        console.info("ROV Data Channel Is OPEN!")
+        // Receive messages
+        rovDataConnection.on('data', function (data) {
+            data = decoder.decode(data);
+            console.log("Received: ", data);
+            document.body.appendChild(document.createTextNode(String(data)));
+        });
+
+        // setInterval(() => {
+        //     console.info("DC Active", rovDataConnection.peerConnection)
+        // }, 1000)
+    });
+    rovDataConnection.on('error', function (err) {
+        console.dir('Remote Peerjs Error: ', err);
+        reconnect()
+    });
+    thisPeer.on('error', function (err) {
+        if (err.type == "peer-unavailable" && thisPeer.open) reconnect() //loop back and try again
+        else abortConnection(err)
+    })
+    rovDataConnection.on('disconnected', function () {
+        console.log('Remote Peerjs disconnected.');
+        console.log("attempting to reconnect to rov peer ..")
+        try {
+            rovDataConnection.close()
+            rovVideoConnection.close()
+        } catch (e) {
+            console.warn("error closing peer: ", e)
+        }
+        reconnect()
+    });
+    rovDataConnection.on('close', function () {
+        console.log('Remote Peerjs connection closed.');
+        try {
+            rovDataConnection.close()
+            rovVideoConnection.close()
+        } catch (e) {
+            console.warn("error closing peer: ", e)
+        }
+        reconnect()
+    });
+}
+
+function reliablyConnectToROV(peerId) {
+    // Using promises as a loop to avoid stack overflow when reconnecting https://stackoverflow.com/questions/50421769/recursive-promises-can-cause-stack-overflow
+    return new Promise(connectToRov).then(() => {
+        console.log("remote peer resolve", thisPeer)
+        // the resolve() callback in the promise function above should get called whenever there is an error connecting to the rov peer, where we should try to reconnect,
+        // the reject() callback in the promise function above should get called whenever there is an error connecting to the peerjs server, where this function should exit and let the outer function take over,
+        // our strange way of looping in order to avoid call stack overflow / memory leak if this loop runs too much
+        if (thisPeer == null || thisPeer.closed) return;
+        return reliablyConnectToROV(peerId)
+    }).catch(() => {
+        try {
+            if (rovDataConnection) rovDataConnection.close();
+            if (rovVideoConnection) rovVideoConnection.close();
+        } catch (e) {
+            console.warn("Error Closing rov Data Or Media connection: ", e)
+        }
+    })
+}
+
+function setupEventListeners() {
+    window.addEventListener('keypress', () => {
+        if (rovDataConnection == null || rovDataConnection.open == false) {
+            alert("No connection to ROV");
+        } else {
+            var msg = window.prompt("Message:");
+            rovDataConnection.send(encoder.encode(msg));
+        }
+    });
+    document.getElementById("connect_btn").addEventListener('click', () => {
+        if (rovDataConnection != null) rovDataConnection.close()
+        remotePeerId = window.prompt("Remote Peer ID", "SSROV_0");
+        reliablyConnectToPeerServer(peerServerCloudOptions)
+    });
+}
+setupEventListeners();
+
+
 //https://dmitripavlutin.com/timeout-fetch-request/
 async function fetchWithTimeout(url, options = {}) {
     const { timeout = 5000 } = options;
@@ -47,163 +229,38 @@ async function fetchWithTimeout(url, options = {}) {
 }
 
 var rovLocalIp = null
+var rovIpFound = false;
+function rovIsAlive() {
+    rovIpFound = true;
+    console.log("ROVISALIVE!!!")
+}
 function findRovLocalIp() {
     // try to brute force search for raspberrypi's ip address
     console.info("Searching for raspberrypi local ip address...")
-    currentThirdOctet = 0
+    currentThirdOctet = -1
+    var scriptElem = null
     function testIp(ipAddress) {
-        return fetchWithTimeout("http://" + ipAddress).then((response) => {
-            if (response.ok) {
-                console.info("Found Local IP: ", ipAddress)
-                return rovLocalIp
+        return new Promise(function (resolve, reject) {
+            console.info("Testing: ", ipAddress)
+            if (scriptElem) document.body.removeChild(scriptElem)
+            scriptElem = document.createElement("SCRIPT")
+            scriptElem.setAttribute("src", "http://" + ipAddress)
+            document.body.appendChild(scriptElem)
+            setTimeout(function () {
+                resolve()
+            }, 500)
+        }).then(function () {
+            if (rovIpFound == true) {
+                return ipAddress
             } else {
-                console.info("got ip search not ok:", response)
                 currentThirdOctet = (currentThirdOctet + 1) % 255
-                return testIp("192.168." + currentThirdOctet + ".88")
+                return testIp("192.168." + currentThirdOctet + ".88/alive")
             }
-        }).catch((error) => {
-            console.info("got ip search error:", error)
-            currentThirdOctet = (currentThirdOctet + 1) % 255
-            return testIp("192.168." + currentThirdOctet + ".88")
         })
     }
-    testIp("192.168." + currentThirdOctet + ".88").then((localIp) => {
+    testIp("192.168." + 0 + ".88/alive").then((localIp) => {
         rovLocalIp = localIp;
         console.info("ROV IP FOUND! " + rovLocalIp)
     })
 }
-
-
-function reliablyConnectToPeerjsServer() {
-    // Using promises as a loop to avoid stack overflow: https://stackoverflow.com/questions/50421769/recursive-promises-can-cause-stack-overflow
-
-    console.log("starting...");
-    var peer = new Peer();
-
-    peer.on('open', function (id) {
-        console.log("Connection to peerjs server established! our PeerID:", realPeerId);
-
-    });
-    peer.on('error', function (err) {
-        console.log('Self Peerjs Error: ', err);
-    });
-    peer.on('disconnected', function () {
-        console.log('Self Peerjs disconnected.');
-        console.log("attempting to reconnect to peerjs server ..")
-        peer.reconnect(); // not a function
-    });
-    peer.on('close', function () {
-        console.log('Self Peerjs connection closed.');
-    });
-}
-
-var rovDataConnection = null;
-var rovVideoConnection = null;
-function reliablyConnectToROV(peerId) {
-    // Using promises as a loop to avoid stack overflow when reconnecting https://stackoverflow.com/questions/50421769/recursive-promises-can-cause-stack-overflow
-    return new Promise(function (resolve, reject) {
-        rovDataConnection = peer.connect(peerId, {
-            reliable: true,
-            serialization: 'none',
-        });
-        rovDataConnection.on('open', function () {
-            console.log("Connection to peerjs server established! our PeerID:", realPeerId);
-            // Receive messages
-            rovDataConnection.on('data', function (data) {
-                data = decoder.decode(data);
-                console.log("Received: ", data);
-                document.body.appendChild(document.createTextNode(String(data)));
-            });
-        });
-        rovDataConnection.on('error', function (err) {
-            console.log('Remote Peerjs Error: ', err);
-        });
-        rovDataConnection.on('disconnected', function () {
-            console.log('Remote Peerjs disconnected.');
-            console.log("attempting to reconnect to rov peer ..")
-            rovDataConnection.reconnect();
-        });
-        rovDataConnection.on('close', function () {
-            console.log('Remote Peerjs connection closed.');
-        });
-        peer.on('call', function (call) {
-            rovVideoConnection = call;
-            console.log('Received video call from: ' + call.peer, call);
-            call.answer(null, {
-                // sdpTransform: function (sdp) {
-                //     console.log('answer sdp: ', sdp);
-                //     return sdp;
-                // }
-            });
-            call.on('stream', function (remoteStream) {
-                console.log('Received stream from: ' + call.peer, remoteStream);
-                var video = document.getElementById('livestream');
-                // video.src = URL.createObjectURL(remoteStream);
-                video.srcObject = remoteStream;
-                video.autoplay = true
-                video.controls = true
-                // video.play();
-            });
-        });
-    }).then(function () {
-        // the resolve() callback in the promise function above should get called whenever there is an error connecting to the rov peer, where we should try to reconnect,
-        // the reject() callback in the promise function above should get called whenever there is an error connecting to the peerjs server, where this function should exit and let the outer function take over,
-        // our strange way of looping in order to avoid call stack overflow / memory leak if this loop runs too much
-        return reliablyConnectToROV(peerId)
-    })
-}
-
-function setupEventListeners(params) {
-    window.addEventListener('keypress', () => {
-        if (rovDataConnection == null || rovDataConnection.open == false) {
-            alert("No connection to ROV");
-        } else {
-            var msg = window.prompt("Message:");
-            rovDataConnection.send(encoder.encode(msg));
-        }
-    });
-    document.getElementById("connect_btn").addEventListener('click', () => {
-        if (rovDataConnection != null) rovDataConnection.close()
-        var remotePeerId = window.prompt("Remote Peer ID", "SSROV_0");
-        connectToPeer(remotePeerId);
-    });
-}
-setupEventListeners();
-
-
-// peer.on('connection', function (dataChannel) {
-//     console.log("connection from: ", dataChannel);
-//     dataChannel.on('open', function () {
-//         console.log("dataChannel open: ", dataChannel);
-//         dataChannel.send('Hello from pilot!');
-//     });
-
-//     dataChannel.on('data', function (data) {
-//         console.log('Received', data);
-//     });
-
-//     dataChannel.on('close', function () {
-//         console.log('Remote Peerjs connection closed.');
-//     });
-
-//     dataChannel.on('error', function (err) {
-//         console.log('Remote Peerjs Error: ', err);
-//     });
-// });
-
-
-
-
-    // setTimeout(function () {
-    //     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-    //     navigator.getUserMedia({ video: true, audio: true }, function (stream) {
-    //         var call = peer.call('SROV', stream);
-    //         call.on('stream', function (remoteStream) {
-    //             var video = document.getElementById('livestream');
-    //             video.src = URL.createObjectURL(remoteStream);
-    //             video.play();
-    //         });
-    //     }, function (err) {
-    //         console.log('Failed to get local stream', err);
-    //     });
-    // }, 1000);
+// findRovLocalIp()
