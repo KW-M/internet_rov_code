d829162a73082127481a9ae74d53fd66c258117f
Kyle Worcester-Moore <25kylworc@gmail.com>
2022-03-20 10:51:15 -0700

    add drok motor controller,but not in use yet

--- python/motion_controller.py
+++ python/motion_controller.py
@@ -9,11 +9,56 @@ log = logging.getLogger(__name__)
 
 ###################################################
 ############### Motor / GPIO Stuff ################
+
+
+class Drok_Pwm_Motor:
+    """ For the Drok 7A dual DC motor driver (Product SKU: 200206)
+    pin_ena: the raspberry pi pin going to the ENA1 pin on the motor driver (ENA2 if driving the second motor)
+    pin_in1: the raspberry pi pin going to IN1 pin on the motor controller (IN3 if driving the second motor)
+    pin_in2: the raspberry pi pin going to IN2 pin on the motor controller (IN4 if driving the second motor)
+    pigpio_instance: the pigpio library instance to use to drive the pwm / gpio signals from the pi
+    """
+    def __init__(self, pigpio_instance, pin_ena, pin_in1, pin_in2):
+        self.pigpio_instance = pigpio_instance
+        self.pin_ena = pin_ena
+        self.pin_in1 = pin_in1
+        self.pin_in2 = pin_in2
+        self.pigpio_instance.set_mode(self.pin_ena, pigpio.OUTPUT)
+        self.pigpio_instance.set_mode(self.pin_in1, pigpio.OUTPUT)
+        self.pigpio_instance.set_mode(self.pin_in2, pigpio.OUTPUT)
+        # Halt pwm / motor (break mode)
+        self.pigpio_instance.set_PWM_dutycycle(self.pin_ena, 0)
+        self.pigpio_instance.write(self.pin_in1, 0)  # pin LOW (off)
+        self.pigpio_instance.write(self.pin_in2, 0)  # pin LOW (off)
+
+    def set_speed(self, speed):
+        """
+        speed: the speed of the motor between 1 (full forward) and -1 (full reverse)
+        """
+        # https://abyz.me.uk/rpi/pigpio/python.html#set_PWM_dutycycle
+        if (speed > 0):
+            speed = min(speed, 1)  # cap speed at 1 (max)
+            self.pigpio_instance.write(self.pin_in1, 1)  # pin HIGH (on)
+            self.pigpio_instance.write(self.pin_in2, 0)  # pin LOW (off)
+            self.pigpio_instance.set_PWM_dutycycle(self.pin_ena, speed * 255)
+        elif (speed < 0):
+            speed = min(
+                -speed, 1
+            )  # minus to cancel out negative speed value and cap speed at 1 (max)
+            self.pigpio_instance.write(self.pin_in1, 0)  # pin LOW (off)
+            self.pigpio_instance.write(self.pin_in2, 1)  # pin HIGH (on)
+            self.pigpio_instance.set_PWM_dutycycle(self.pin_ena, speed * 255)
+        else:
+            self.pigpio_instance.write(self.pin_in1, 0)  # pin LOW (off)
+            self.pigpio_instance.write(self.pin_in2, 0)  # pin LOW (off)
+            self.pigpio_instance.set_PWM_dutycycle(self.pin_ena, 0)
+
+
 class Adafruit_Pwm_Motor:
-    """ for the adafruit drv8871 single motor controllers
+    """ for the Adafruit drv8871 single motor controller
     pin_in1: the raspberry pi pin going to in1 pin on the motor controller
     pin_in2: the raspberry pi pin going to in2 pin on the motor controller
-    pigpio_instance: the pigpio instance to use to drive the pwm
+    pigpio_instance: the pigpio library instance to use to drive the pwm / gpio signals from the pi
     """
     def __init__(self, pigpio_instance, pin_in1, pin_in2):
         self.pigpio_instance = pigpio_instance
@@ -31,7 +76,7 @@ class Adafruit_Pwm_Motor:
         # https://abyz.me.uk/rpi/pigpio/python.html#set_PWM_dutycycle
         if (speed > 0):
             speed = min(speed, 1)  # cap speed at 1 (max)
-            self.pigpio_instance.write(self.pin_in1, 0)  # PWM off
+            self.pigpio_instance.write(self.pin_in1, 0)  # pin LOW (off)
             # for real motor conrol these should be 1 not 0
             # 254 because 255 means breaking mode on the drv8871
             self.pigpio_instance.set_PWM_dutycycle(self.pin_in2, speed * 254)
@@ -39,11 +84,12 @@ class Adafruit_Pwm_Motor:
             speed = -speed  # cancel out negative speed value
             speed = min(speed, 1)  # cap speed at 1 (max)
             self.pigpio_instance.set_PWM_dutycycle(self.pin_in1, speed * 254)
-            self.pigpio_instance.write(self.pin_in2, 0)  # PWM off
+            self.pigpio_instance.write(self.pin_in2, 0)  # pin LOW (off)
             # for real motor conroll these should be 1 not 0
         else:
-            self.pigpio_instance.write(self.pin_in1, 0)  # PWM off
-            self.pigpio_instance.write(self.pin_in2, 0)  # PWM off
+            self.pigpio_instance.write(self.pin_in1, 0)  # pin LOW (off)
+            self.pigpio_instance.write(self.pin_in2, 0)  # pin LOW (off)
+
 
 class Motion_Controller:
 
