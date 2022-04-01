4fbfe6d2348aaabb660d987950b2955f2bc1f5b8
Kyle Worcester-Moore <25kylworc@gmail.com>
2022-03-31 21:58:31 -0700

    compiling fixes

--- arduino_cpp/compile_arduino_code.sh
+++ arduino_cpp/compile_arduino_code.sh
@@ -7,9 +7,11 @@ FOLDER_CONTAINING_THIS_SCRIPT=${PATH_TO_THIS_SCRIPT%/*}
 # change directory into the arduino_cpp directory
 pushd "$FOLDER_CONTAINING_THIS_SCRIPT";
 
+pwd;
+
 # compile the arduino code
-g++ -c ./arduPi/arduPi.cpp -o ./arduPi/arduPi.o
-g++ -lrt -lpthread main.cpp ./arduPi/arduPi.o -o arduino_program
+g++ -c ./libraries/arduPi/arduPi.cpp -o ./libraries/arduPi/arduPi.o
+g++ -lrt -lpthread main.cpp ./libraries/arduPi/arduPi.o -o arduino_program
 
 # change directory back to what it was previously
 popd;
