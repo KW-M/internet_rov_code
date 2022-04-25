4ca796bddd6ab2e8c038d0bf0fd7f9f357ce47e4
Kyle Worcester-Moore <25kylworc@gmail.com>
2022-04-24 17:01:17 -0700

    media debug

--- go/main.go
+++ go/main.go
@@ -61,14 +61,14 @@ func main() {
 	}()
 
 	// Create the two named pipes to send and receive data to / from the webrtc-relay user's backend code
-	print("Creating named pipe relay...", config.NamedPipeFolder)
-	os.MkdirAll(config.NamedPipeFolder, os.ModePerm)
-	msgPipe, err = CreateDuplexNamedPipeRelay(config.NamedPipeFolder+"to_datachannel_relay.pipe", config.NamedPipeFolder+"from_datachannel_relay.pipe", 4096)
-	if err != nil {
-		log.Fatal("Failed to create message relay named pipe: ", err)
-	}
-	defer msgPipe.Close()
-	go msgPipe.runPipeLoops(programShouldQuitSignal)
+	// print("Creating named pipe relay...", config.NamedPipeFolder)
+	// os.MkdirAll(config.NamedPipeFolder, os.ModePerm)
+	// msgPipe, err = CreateDuplexNamedPipeRelay(config.NamedPipeFolder+"to_datachannel_relay.pipe", config.NamedPipeFolder+"from_datachannel_relay.pipe", 4096)
+	// if err != nil {
+	// 	log.Fatal("Failed to create message relay named pipe: ", err)
+	// }
+	// defer msgPipe.Close()
+	// go msgPipe.runPipeLoops(programShouldQuitSignal)
 
 	// initVideoTrack()
 	// go pipeVideoToStream(programShouldQuitSignal)
